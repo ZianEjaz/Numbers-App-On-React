11e2429bf103814c05d835ca429eebd418dd4371
Zian Ejaz <zianejaz@gmail.com>
2021-10-13 23:51:10 +0500

    arranged data from file reader component to home.

--- src/components/fileReader.js
+++ src/components/fileReader.js
@@ -1,22 +1,15 @@
 import React, { Component } from "react";
 import CSVReader from "react-csv-reader";
-import DataDisplay from "./dataDisplay";
-import Search from "./search";
-
-
 
 class FileReader extends Component {
   state = {
-    fileReadSucessfully : false,
-    numbersArray: [],
-    fileInfo: "",
-    filtered: [],
-    keyword: "",
-    data: []
+    fileData: {
+      fileReadSucessfully: false,
+      fileInfo: "",
+      data: [],
+      numbersArray: [],
+    },
   };
-
-
-
 
   papaparseOptions = {
     header: true,
@@ -31,74 +24,28 @@ class FileReader extends Component {
       return numbersArray.push(data.numbers);
     });
     this.setState({
-      fileReadSucessfully : true,
+      fileReadSucessfully: true,
       numbersArray,
       fileInfo,
-      data
+      data,
     });
-    this.props.passDataToHome(numbersArray);
-  };
-
-  searchNumbers = (event) => {
-    let keyword = event.target.value;
-    let filtered = this.state.numbersArray.filter((item) => {
-      return item.toString().indexOf(keyword) > -1;
-    });
-    this.setState({
-      filtered,
-      keyword,
-    });
+    this.props.passFileDataToHomeState(this.state.fileData);
   };
 
   render() {
     return (
-      <div className="flex flex-wrap justify-center">
-        <div className="flex flex-wrap text-white text-xl w-full lg:w-1/4 content-start">
-          <div className="p-5 mt-8 bg-purple-600 rounded-2xl w-full">
-            <Search searchNumbers={this.searchNumbers} />
-          </div>
-          <div className="p-5 mt-8 bg-purple-600 rounded-2xl w-full">
-            <p>Upload a CSV file to import Contacts</p>
-
-   
-
-            <CSVReader
-              onFileLoaded={this.readFiletoState}
-              parserOptions={this.papaparseOptions}
-              cssClass="csv-reader-parent-class w-full text-center mt-6 flex flex-col jus items-center bg-white rounded-md shadow-md tracking-wide uppercase border border-blue cursor-pointer hover:bg-purple-600 hover:text-white text-purple-600 ease-linear transition-all duration-150"
-              cssInputClass="hidden"
-              label = "Select file"
-            />
-            <div>
+      <>
+        <CSVReader
+          onFileLoaded={this.readFiletoState}
+          parserOptions={this.papaparseOptions}
+          cssClass="csv-reader-parent-class w-full text-center mt-6 flex flex-col jus items-center bg-white rounded-md shadow-md tracking-wide uppercase border border-blue cursor-pointer hover:bg-purple-600 hover:text-white text-purple-600 ease-linear transition-all duration-150"
+          cssInputClass="hidden"
+          label="Select file"
+        />
+        <div>
               {this.state.fileReadSucessfully === true ? <p>File Sucessfully Uploaded</p> : <></>}
             </div>
-          </div>
-        </div>
-        <div className="text-white bg-purple-700 p-8 rounded-2xl mt-8 w-full lg:w-2/3 m-auto relative animate__bounceInDown">
-
-        
-
-          <h3 className="text-2xl">File Name: {this.state.fileInfo.name}</h3>
-          <h3>File Type: {this.state.fileInfo.type}</h3>
-
-          {this.state.keyword === "" ? (
-            <h3>Total Numbers Imported {this.state.numbersArray.length}</h3>
-          ) : (
-            <h3  className="text-2xl p-4 rounded m-5">
-              Total Numbers Found:
-              <span className="m-4">{this.state.filtered.length}</span>
-            </h3>
-          )}
-           
-          {this.state.keyword === "" ? (
-            <DataDisplay data={this.state.numbersArray} />
-          ) : (
-            <DataDisplay data={this.state.filtered} />
-          )}
-
-          <div className=" "></div>
-        </div>
-      </div>
+      </>
     );
   }
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,21 +1,78 @@
 import React, { Component } from "react";
 import FileReader from "./fileReader";
+import DataDisplay from "./dataDisplay";
+import Search from "./search";
 
 class Home extends Component {
   state = {
-    data : null
+    fileData : {},
+    filtered: [],
+    keyword: "",
   }
 
-  passDataToHome = (childData) =>{
-    this.setState({data: childData})
+  passFileDataToHomeState = (childData) =>{
+    this.setState({fileData: childData})
 }
 
-
+searchNumbers = (event) => {
+  let keyword = event.target.value;
+  let filtered = this.state.fileData.numbersArray.filter((item) => {
+    return item.toString().indexOf(keyword) > -1;
+  });
+  this.setState({
+    filtered,
+    keyword,
+  });
+};
   render() {
     return (
       <div className="w-4/6 m-auto">
-        {console.log(`from home component ${this.state.data}`)}
-        <FileReader passDataToHome={this.passDataToHome}/>
+
+
+<div className="flex flex-wrap justify-center">
+        <div className="flex flex-wrap text-white text-xl w-full lg:w-1/4 content-start">
+          <div className="p-5 mt-8 bg-purple-600 rounded-2xl w-full">
+            <Search searchNumbers={this.searchNumbers} />
+          </div>
+          <div className="p-5 mt-8 bg-purple-600 rounded-2xl w-full">
+            <p>Upload a CSV file to import Contacts</p>
+
+   
+
+            <FileReader passFileDataToHomeState={this.passFileDataToHomeState}/>
+            
+          </div>
+        </div>
+        <div className="text-white bg-purple-700 p-8 rounded-2xl mt-8 w-full lg:w-2/3 m-auto relative animate__bounceInDown">
+
+        
+
+          <h3 className="text-2xl">File Name: {this.state.fileData.fileInfo.name}</h3>
+          <h3>File Type: {this.state.fileInfo.type}</h3>
+
+          {this.state.keyword === "" ? (
+            <h3>Total Numbers Imported {this.state.fileData.numbersArray.length}</h3>
+          ) : (
+            <h3  className="text-2xl p-4 rounded m-5">
+              Total Numbers Found:
+              <span className="m-4">{this.state.filtered.length}</span>
+            </h3>
+          )}
+           
+          {this.state.keyword === "" ? (
+            <DataDisplay data={this.state.fileData.numbersArray} />
+          ) : (
+            <DataDisplay data={this.state.filtered} />
+          )}
+
+          <div className=" "></div>
+        </div>
+      </div>
+
+
+
+{/* ------------------------------- */}
+        
       </div>
     );
   }
